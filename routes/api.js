d044a40f532dbb007868998402f8bc4063bcb15b
Anand Raj <anandamritraj904@gmail.com>
2020-03-24 11:26:49 +0530

    Add testing data

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,12 @@ const axios = require("axios");
 let currentData = {
   total: {},
   statewise: {},
-  dayChange: {}
+  dayChange: {},
+  tested: {}
+};
+
+let tested = {
+  tested: []
 };
 
 const stateCodeAndNameMap = {
@@ -49,65 +54,105 @@ const stateCodeAndNameMap = {
   Lakshadweep: "ld"
 };
 
-const fetchDataFromSource = () => {
+const fetchStateWiseDataFromSource = () => {
   console.log("Fetching Data from source...");
 
   axios
     .get("https://api.covid19india.org/data.json")
-    .then(({ data }) => {
-      const stateData = data.statewise;
-      let max = 0;
-      let min = Infinity;
-      let currentDataNew = {
-        total: {},
-        statewise: {},
-        dayChange: {}
-      };
-      stateData.map(state => {
-        if (state.state === "Total") {
-          currentDataNew.total = {
-            code: stateCodeAndNameMap[state.state],
-            name: state.state,
-            active: state.active,
-            confirmed: state.confirmed,
-            deaths: state.deaths,
-            recovered: state.recovered || 0,
-            lastUpdated: Date.now()
-          };
-        } else {
-          currentDataNew.statewise[stateCodeAndNameMap[state.state]] = {
-            code: stateCodeAndNameMap[state.state],
-            name: state.state,
-            active: state.active,
-            confirmed: state.confirmed,
-            deaths: state.deaths,
-            recovered: state.recovered || 0
-          };
-          max =
-            parseInt(state.confirmed) > max ? parseInt(state.confirmed) : max;
-          min =
-            parseInt(state.confirmed) < min ? parseInt(state.confirmed) : min;
-        }
-      });
-      currentDataNew.total.max = max;
-      currentDataNew.total.min = min;
-      currentDataNew.dayChange = {
-        confirmed: data.key_values[0].confirmeddelta,
-        deceased: data.key_values[0].deceaseddelta,
-        recovered: data.key_values[0].recovereddelta
-      };
-      currentData = currentDataNew;
+    .then(result => {
+      if (result.status === 200) {
+        let data = result.data;
+        const stateData = data.statewise;
+        let max = 0;
+        let min = Infinity;
+        let currentDataNew = {
+          total: {},
+          statewise: {},
+          dayChange: {},
+          tested: {}
+        };
+        stateData.map(state => {
+          if (state.state === "Total") {
+            currentDataNew.total = {
+              code: stateCodeAndNameMap[state.state],
+              name: state.state,
+              active: state.active,
+              confirmed: state.confirmed,
+              deaths: state.deaths,
+              recovered: state.recovered || 0,
+              lastUpdated: Date.now()
+            };
+          } else {
+            currentDataNew.statewise[stateCodeAndNameMap[state.state]] = {
+              code: stateCodeAndNameMap[state.state],
+              name: state.state,
+              active: state.active,
+              confirmed: state.confirmed,
+              deaths: state.deaths,
+              recovered: state.recovered || 0
+            };
+            max =
+              parseInt(state.confirmed) > max ? parseInt(state.confirmed) : max;
+            min =
+              parseInt(state.confirmed) < min ? parseInt(state.confirmed) : min;
+          }
+        });
+        // Assign Current Data
+        currentDataNew.total.max = max;
+        currentDataNew.total.min = min;
+        currentDataNew.dayChange = {
+          confirmed: data.key_values[0].confirmeddelta,
+          deceased: data.key_values[0].deceaseddelta,
+          recovered: data.key_values[0].recovereddelta
+        };
+        const previousTested =
+          data.tested[data.tested.length - 3].totalsamplestested;
+        const currentTested =
+          data.tested[data.tested.length - 1].totalsamplestested;
+        currentDataNew.tested = data.tested[data.tested.length - 1];
+        currentDataNew.tested.delta = currentTested - previousTested;
+        currentData = currentDataNew;
+
+        // Assign Tested
+        tested = data.tested;
+      } else {
+        console.log("There was an error in the backend api");
+      }
     })
     .catch(err => {
       console.log(err);
     });
 };
 
-setInterval(fetchDataFromSource, 1000 * 60 * 5);
-fetchDataFromSource();
+const fetchLiveBlogDataFromSource = index => {
+  console.log("Fetching data from backend");
+  index = index + 1;
+  axios
+    .get(
+      `https://economictimes.indiatimes.com/etstatic/liveblogs/msid-74765889,callback-liveBlogTypeALL-${index}.htm`
+    )
+    .then(result => {
+      if (result.status === 200) {
+        console.log(result.data);
+        fetchLiveBlogDataFromSource(index + 1);
+      } else {
+        console.log("There was an error from backend");
+      }
+    })
+    .catch(err => {
+      console.log("There was an error in the API");
+    });
+};
+setInterval(fetchStateWiseDataFromSource, 1000 * 60 * 5);
+// fetchLiveBlogDataFromSource(0);
+fetchStateWiseDataFromSource();
 
 router.get("/state", function(req, res, next) {
   res.json(currentData);
+});
+
+router.get("/tested", function(req, res, next) {
+  res.json(tested);
 });
 
 module.exports = router;
