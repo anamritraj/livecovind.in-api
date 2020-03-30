6d57a5fa8b10ce6b4801ed02c6652806cfa59165
Anand Raj <anandamritraj904@gmail.com>
2020-03-30 19:52:17 +0530

    Don't allow request from any other domains

--- app.js
+++ app.js
@@ -12,6 +12,25 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+function checkOrigin(origin) {
+  console.log("origin: ", (origin ? origin : "DIRECT API CALL"));
+  if (!origin || origin.includes("livecovid.in") > 0 || origin.includes("localhost")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+app.use(function(req, res, next) {
+  if (checkOrigin(req.get('origin'))) {
+    next();
+  } else {
+    res.sendStatus(403);
+    res.end();
+  }
+});
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
