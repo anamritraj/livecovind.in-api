822950acedd2ef6817577e6dac9cc3bc8709e573
Anand Raj <anandamritraj904@gmail.com>
2020-05-02 20:10:09 +0530

    Add timeseries data for timeseries map explorer

--- services/timeseries.service.js
+++ services/timeseries.service.js
@@ -1,30 +1,68 @@
 const moment = require("moment");
 
+
 const { getAllDataFromFirebase } = require('./firebase-service');
 let indianStateTimeseriesData = {};
 let raceChartData = {};
 
+const getMaxObjectForDay = (data) => {
+  const maxValues = {
+    deaths: -1,
+    active: -1,
+    confirmed: -1,
+    recovered: -1,
+  }
+  Object.keys(data).map(stateKey => {
+    if (stateKey !== 'total'){
+      maxValues.deaths = Math.max(data[stateKey].value.deaths, maxValues.deaths);
+      maxValues.confirmed = Math.max(data[stateKey].value.confirmed, maxValues.confirmed);
+      maxValues.active = Math.max(data[stateKey].value.active, maxValues.active);
+      maxValues.recovered = Math.max(data[stateKey].value.recovered, maxValues.recovered);
+    }
+  })
+  console.log(maxValues);
+  return maxValues;
+}
 const updateIndianStatesTimeSeriesData = () => {
   // eslint-disable-next-line no-undef
   getAllDataFromFirebase('stateWiseTimeSeries').then((data) => {
-    indianStateTimeseriesData = data;
     // Time to prepare data for raceCharts
     const barDataNew = [];
+    const timeSeriesDatanew = [];
     Object.keys(data).forEach((key) => {
       // This is the data for one of the dates.
       // Need to convert it into {stateid, value}
-      let dayData = Object.keys(data[key]).map(stateKey => {
+
+      let raceDayData = Object.keys(data[key]).map(stateKey => {
         return {
           id: data[key][stateKey].name,
           value: data[key][stateKey].confirmed
         }
       })
+
+      let timeSeriesDayData = {};
+      Object.keys(data[key]).forEach(stateKey => {
+        timeSeriesDayData[stateKey] = {
+          name: data[key][stateKey].name,
+          value: data[key][stateKey]
+        }
+      })
+
       barDataNew.push({
         date: moment(key, "DD-MM-YYYY"),
-        data: dayData
+        data: raceDayData
+      })
+
+      timeSeriesDatanew.push({
+        date: moment(key, "DD-MM-YYYY"),
+        data: timeSeriesDayData
       })
     })
+
     let barDataSorted = barDataNew.sort((a, b) => a.date.isAfter(b.date) ? 1 : -1);
+
+    let timeSeriesDataSorted = timeSeriesDatanew.sort((a, b) => a.date.isAfter(b.date) ? 1 : -1);
+
     barDataSorted = barDataSorted.map(daysData => {
       daysData.data.sort((a, b) => {
         if (a.id === 'Total') return -1;
@@ -37,7 +75,15 @@ const updateIndianStatesTimeSeriesData = () => {
         data: daysData.data.slice(daysData.data.length - 6, daysData.data.length)
       }
     })
+    timeSeriesDataSorted = timeSeriesDataSorted.map(daysData => {
+      return {
+        date: daysData.date.format(),
+        data: daysData.data,
+        max: getMaxObjectForDay(daysData.data)
+      }
+    });
     raceChartData = barDataSorted;
+    indianStateTimeseriesData = timeSeriesDataSorted;
   })
 }
 
